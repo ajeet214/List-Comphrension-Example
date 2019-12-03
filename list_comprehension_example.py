17b34d07d8e0e102155bb10dd383f54519511be7
ajeet   verma <ajeet214.90@gmail.com>
2019-12-03 10:38:58 +0530

    Create list_comprehension_example.py

---
+++ list_comprehension_example.py
@@ -0,0 +1,15 @@
+C = [2, 5, 9, 12, 13, 15, 16, 17, 18, 19]
+F = [2, 4, 5, 6, 7, 9, 13, 16]
+H = [1, 2, 5, 9, 10, 11, 12, 13, 15]
+
+# Write your code here
+all_sports = [i for i in C if i in F and i in H]
+cricket_and_football = [i for i in C if i in F and i not in H]
+do_not_play_any = [i for i in range(1, 21) if (i in C and i in F and i not in H) or (i in F and i in H and i not in C) or (i in C and i in H and i not in F)]
+two_sports = [i for i in range(1, 21) if i not in C and i not in F and i not in H]
+
+# (Make sure your lists are sorted in ascending order before you print them out)
+print(sorted(all_sports))
+print(sorted(cricket_and_football))
+print(sorted(do_not_play_any))
+print(sorted(two_sports))
